4506db5f9d2d9a3fc6707b41e2079f4bad47e60a
dshapalov <dshapalov@mail.ru>
2021-05-18 17:07:15 +0500

    Test 1
    Test 3

---
+++ Palindrome/src/Palindrome.java
@@ -0,0 +1,24 @@
+//Write a program that prompts the user to enter a three-digit
+//integer and determines whether it is a palindrome number. 
+//A number is palindrome if it reads the same from right to left 
+//and from left to right. Here is a sample run of this program:
+
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		//Test 2
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
